3bf90c00c52a0ea20e2872484bd5ab3674c93565
Neil Kidd <neil.a.kidd@gmail.com>
2020-09-15 18:34:46 +0100

    Make emails indexed and lowercase

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,7 @@ class User < ApplicationRecord
     validates :email, presence: true,
                       length: { maximum:255 },
                       format: { with: VALID_EMAIL_REGEX },
-                      uniqueness: { case_sensitive: false }
+                      uniqueness: true
+
+    before_save { email.downcase! }
 end
