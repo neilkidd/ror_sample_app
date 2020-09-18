bf90710d26e109227ded9da287ff6fcbc04170fe
Neil Kidd <neil.a.kidd@gmail.com>
2020-09-18 18:30:40 +0100

    Building a user profile page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  get 'users/new'
   root 'static_pages#home'
 
   get '/about',   to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
   get '/help',    to: 'static_pages#help'
   get '/signup',  to: 'users#new'
-  
+
+  get 'users/new'
+  resources :users
 end
